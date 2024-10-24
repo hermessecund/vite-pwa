2a34fac222045b6856c132d5a901d4c13847d86a
Tarwar <69816954+hermessecund@users.noreply.github.com>
2024-10-24 10:53:54 +0000

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -2,12 +2,9 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
-import { ThirdwebProvider } from 'thirdweb/react'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <ThirdwebProvider>
-      <App />
-    </ThirdwebProvider>
+    <App />
   </React.StrictMode>,
 )
